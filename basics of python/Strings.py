b7b24439e4b259f04b4ecda53d49e5acd722904f
Samar AYADI <ayadisamardev@gmail.com>
2023-08-15 10:14:08 +0100

    Strings

---
+++ basics of python/Strings.py
@@ -0,0 +1,17 @@
+platform = """PinguCoder
+Platform n1
+Samar AYADI
+"""
+print(platform)
+
+
+platform_one = "Pingu Coder"
+print(len(platform_one))
+print(platform_one[0])
+print(platform_one[10])
+print(platform_one[0:5])
+print(platform_one[0:])
+print(platform_one[:11])
+print(platform_one[0:-1])
+print(platform_one[-12:-6])
+
