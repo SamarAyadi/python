c2d222626963c72a0db7ddb54f7a4bc4752d3368
Samar AYADI <ayadisamardev@gmail.com>
2023-08-15 10:04:03 +0100

    Variables

--- basics of python/firstProgram.py
+++ basics of python/firstProgram.py
@@ -1,2 +1,9 @@
-print("Salam mes chers developpeurs")
+print ("Salam mes chers developpeurs")
 print("_" * 20)
+
+price = 500
+ratting = 4.99
+valid = True
+title = "Pingu Coder"
+
+course_price = ""
