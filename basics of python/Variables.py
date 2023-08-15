ebc001761669b9be335a4beef4524b8e58f05bb5
Samar AYADI <ayadisamardev@gmail.com>
2023-08-15 10:05:41 +0100

    variables

---
+++ basics of python/Variables.py
@@ -0,0 +1,7 @@
+
+price = 500
+ratting = 4.99
+valid = True
+title = "Pingu Coder"
+
+course_price = ""
--- basics of python/firstProgram.py
+++ basics of python/firstProgram.py
@@ -1,9 +1,2 @@
 print ("Salam mes chers developpeurs")
 print("_" * 20)
-
-price = 500
-ratting = 4.99
-valid = True
-title = "Pingu Coder"
-
-course_price = ""
